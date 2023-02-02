1341d5b8801e121f1a6fb66f8cbeda181fa3730e
Yegenie1 <124111019+Yegenie1@users.noreply.github.com>
2023-02-02 17:44:39 +0900

    js

---
+++ day04/out.js
@@ -0,0 +1,21 @@
+function 빨래하다() {
+
+    console.log("1. 먼지를 털자.")
+    console.log("2. 물에 집에 넣는다.")
+    console.log("3. 비누를 바른다.")
+    console.log("4. 오물조물 비빈다.")
+    console.log("5. 물에 여러번 헹군다.")
+    console.log("6. 짠다.")
+    console.log("7. 넌다.")
+}
+
+//청소하다
+
+function 청소하다() {
+    document.write("창문을 연다.<br>")
+    document.write("옷을 정리한다.<br>")
+    document.write("책상을 정리한다.<br>")
+    document.write("청소기를 돌린다.<br>")
+    document.write("물걸레질을 한다.<br>")
+
+}
---
+++ day04/out2.js
@@ -0,0 +1,12 @@
+function 여행하다() {
+    document.write("계획을 짠다<br>")
+    document.write("여행을 간다<br>")
+
+}
+
+function 쉬다() {
+    console.log("책상에서 벗어난다")
+    console.log("스트레칭을 한다")
+    console.log("눕는다")
+
+}
